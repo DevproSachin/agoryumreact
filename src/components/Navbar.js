a7aff013b7d5f63f48e062d9b5d8972102d7315d
Sachin Sharma <85942650+DevproSachin@users.noreply.github.com>
2022-09-04 17:08:09 +0530

    update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
             <header>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container">
-                        <a className="navbar-brand" href="index.html"><img src={logo} alt="" height="40" /></a>
+                        <Link className="navbar-brand" to="index.html"><img src={logo} alt="" height="40" /></Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
